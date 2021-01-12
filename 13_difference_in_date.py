5717f8f17fdf6007846f7efc0c2c1d3a1823940f
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-01-12 09:02:08 +0530

    Create 13_difference_in_date.py

---
+++ 13_difference_in_date.py
@@ -0,0 +1,1 @@
+""" Design a Python script to determine the difference in date for given two dates in YYYY:MM:DD format (0 <= YYYY <= 9999, 1 <= MM <= 12, 1 <= DD <= 31) following the leap year rules. """
