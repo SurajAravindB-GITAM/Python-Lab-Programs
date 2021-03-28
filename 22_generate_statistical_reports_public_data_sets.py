6bb95bdeacddab6a1c6d88c8461dcca940cda2c9
Rakesh-2002 <75826932+Rakesh-2002@users.noreply.github.com>
2021-03-28 15:47:02 +0530

    Update 22_generate_statistical_reports_public_data_sets.py

--- 22_generate_statistical_reports_public_data_sets.py
+++ 22_generate_statistical_reports_public_data_sets.py
@@ -1,1 +1,19 @@
 """ Design a Python script to generate statistical reports (Minimum, Maximum, Count, Average, Sum etc) on public datasets. """
+import pandas as pd
+file = r"C:\Python36\rakesh\marks.xlsx"
+data = pd.read_excel(file,usecols = "D")
+list1 = data.values.tolist()
+details = []
+for i in list1:
+    for j in i:
+        details.append(j)
+print("The maximum marks is: ",max(details))
+max_marks = details.count(max(details))
+print("The minimum marks is: ",min(details))
+min_marks = details.count(min(details))
+max_marks = details.count(max(details))
+print("count of min:",min_marks)
+print("count of max:",max_marks)
+print("Test written members:",len(details))
+average_marks = sum(details)/len(details)
+print(average_marks)
