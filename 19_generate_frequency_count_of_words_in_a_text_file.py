458079e436933ad6670a3d3490d0133fd48bac84
Rakesh-2002 <75826932+Rakesh-2002@users.noreply.github.com>
2021-03-28 16:15:22 +0530

    Update 19_generate_frequency_count_of_words_in_a_text_file.py

--- 19_generate_frequency_count_of_words_in_a_text_file.py
+++ 19_generate_frequency_count_of_words_in_a_text_file.py
@@ -1,1 +1,7 @@
 """ Design a Python Script to generate the frequency count of words in a text file. """
+from collections import Counter
+def word_count(fname):
+        with open(fname) as f:
+                return Counter(f.read().split())
+
+print("Number of words in the file :",word_count("test.txt"))
